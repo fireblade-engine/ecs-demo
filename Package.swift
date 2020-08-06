20fbef2da8edd3306055dc0ab899535cd6de9cd8
Christian Treffs <ctreffs@gmail.com>
2020-08-06 21:53:28 +0200

    Update to ECS 0.15.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 		.package(url: "https://github.com/ctreffs/SwiftSDL2.git", from: "1.1.0"),
-		.package(url: "https://github.com/fireblade-engine/ecs.git", from: "0.14.0")
+		.package(url: "https://github.com/fireblade-engine/ecs.git", from: "0.15.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
