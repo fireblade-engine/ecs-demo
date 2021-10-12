a2c3908043819934a615904fabcaf2bb3783bef7
Christian Treffs <ctreffs@gmail.com>
2021-10-12 15:15:29 +0200

    Update dependencies to official repos

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -11,15 +9,14 @@ let package = Package(
         .tvOS(.v11)
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(name: "SDL2", url: "git@github.com:pcbeard/SwiftSDL2.git", .branch("vcpkg-sdl")),
-        .package(name: "FirebladeECS", url: "https://github.com/fireblade-engine/ecs.git", from: "0.17.4"),
-        .package(name: "FirebladeMath", url: "git@github.com:pcbeard/math.git", .branch("Windows"))
+        .package(name: "SDL2", url: "https://github.com/ctreffs/SwiftSDL2.git", from: "1.3.0"),
+        .package(name: "FirebladeECS", url: "https://github.com/fireblade-engine/ecs.git", from: "0.17.5"),
+        .package(name: "FirebladeMath", url: "https://github.com/fireblade-engine/math.git", from: "0.11.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "SDLKit", dependencies: ["SDL2"]),
+        .target(
+            name: "SDLKit",
+            dependencies: ["SDL2"]),
         .target(
             name: "Particles",
             dependencies: ["FirebladeECS", "SDL2", "SDLKit"]),
